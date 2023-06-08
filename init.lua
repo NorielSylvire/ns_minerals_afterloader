96788ce255dd2853ea260690f060eca5a4fc1bf4
NorielSylvire <fl4v1u5@yandex.com>
2023-06-08 20:07:40 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,6 @@
+if not minetest.get_modpath("ns_minerals_core") then
+	minetest.log("ns_minerals_core mod is not installed properly. The modpack may not function correctly.")
+end
+if not minetest.get_modpath("nslib") then
+	minetest.log("nslib mod is not installed properly. The modpack may not function correctly.")
+end
